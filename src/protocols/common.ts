42be2683aa5e4ea0ade2d2b515d11d405b312d0d
Kimmy <kenpusney@gmail.com>
2019-11-15 19:21:39 +0800

    add null guard

--- src/protocols/common.ts
+++ src/protocols/common.ts
@@ -5,6 +5,7 @@ export function processWithMutableState<Param, State>(handler: ExtendedHandler<P
   const state = handler.state();
 
   const result = handler.process.call(handler, state, param)
-
-  handler.state({ ...state, ...result });
+  if (result) {
+    handler.state({ ...state, ...result });
+  }
 }
